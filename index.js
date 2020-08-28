8dbff25c7426b2874f9272441cef9036aad7aa87
wesleyrocha90 <wesleyallanrocha@gmail.com>
2020-08-27 22:37:22 -0300

    Decoding opcodes

--- index.js
+++ index.js
@@ -1,4 +1,49 @@
 (function gameLoop() {
 
-    console.log('game loop');
+    // variables
+    var memory = new Array(4096);
+    var V = new Array(16);
+    var I;
+    var keyboard = new Array(16);
+    var screen = new Array(64 * 32);
+
+    // delay timer register
+    var DT;
+    // sound timer register
+    var ST;
+
+    function loadGame() {
+        let inputFile = document.getElementById('file');
+        let address = 512;
+
+        inputFile.addEventListener('change', (event) => {
+            var reader = new FileReader();
+            reader.onloadend = () => {
+                var data = reader.result;
+                
+                var array = new Uint8Array(data);
+
+                array.forEach(byte => memory[address++]);
+
+                console.log(fileByteArray);
+            };
+            reader.readAsArrayBuffer(inputFile.files[0]);
+        });
+    }
+
+    function gameLoop() {
+
+        emulateCycle();
+
+        // draw graphics
+
+        // read inputs
+
+    }
+
+    function emulateCycle() {
+        // fetch opcode
+        // decode opcode
+        // execute opcode
+    }
 })()
